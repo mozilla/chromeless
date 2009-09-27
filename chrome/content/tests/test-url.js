56665f2b7ed546b37406acaf38fa124636b2210a
Atul Varma <varmaa@toolness.com>
2009-09-27 13:21:33 -0700

    added another url test

--- chrome/content/tests/test-url.js
+++ chrome/content/tests/test-url.js
@@ -7,11 +7,15 @@ exports.testUrl = function(test) {
   test.assertEqual(url.resolve("http://www.foo.com", "bar"),
                    "http://www.foo.com/bar");
 
-  test.assertEqual(url.resolve("http://foo.com", "http://bar.com"),
+  test.assertEqual(url.resolve("http://foo.com/", "http://bar.com/"),
                    "http://bar.com/",
                    "relative should override base");
 
   test.assertRaises(function() { url.resolve("blah"); },
                     "malformed URI: blah",
                     "url.resolve() should throw malformed URI on base");
+
+  test.assertEqual(url.resolve("http://www.foo.com", ""),
+                   "http://www.foo.com/",
+                   "url.resolve() should add slash to end of domain");
 };
