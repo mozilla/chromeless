57cd071ae31c00a160dd73af4a42df5b7971d591
Atul Varma <varmaa@toolness.com>
2009-09-27 17:03:58 -0700

    Minor refactoring to url.js.

--- chrome/content/lib/url.js
+++ chrome/content/lib/url.js
@@ -1,11 +1,15 @@
 var ios = Cc['@mozilla.org/network/io-service;1']
           .getService(Ci.nsIIOService);
 
-var resolve = exports.resolve = function resolve(base, relative) {
+function newURI(uriStr) {
   try {
-    var baseURI = ios.newURI(base, null, null);
+    return ios.newURI(uriStr, null, null);
   } catch (e if e.result == Cr.NS_ERROR_MALFORMED_URI) {
-    throw new Error("malformed URI: " + base);
+    throw new Error("malformed URI: " + uriStr);
   }
+}
+
+var resolve = exports.resolve = function resolve(base, relative) {
+  var baseURI = newURI(base);
   return ios.newURI(relative, null, baseURI).spec;
 };
