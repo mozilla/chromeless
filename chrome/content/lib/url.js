5c7a8dc87a883692f36ee0adf36183ebcce2a7d9
Atul Varma <varmaa@toolness.com>
2009-09-27 19:36:19 -0700

    Added exception for invalid URIs

--- chrome/content/lib/url.js
+++ chrome/content/lib/url.js
@@ -6,8 +6,19 @@ function newURI(uriStr) {
     return ios.newURI(uriStr, null, null);
   } catch (e if e.result == Cr.NS_ERROR_MALFORMED_URI) {
     throw new Error("malformed URI: " + uriStr);
+  } catch (e if e.result == Cr.NS_ERROR_FAILURE) {
+    throw new Error("invalid URI: " + uriStr);
   }
 }
+
+var parse = exports.parse = function parse(url) {
+  var uri = newURI(url);
+  return {scheme: uri.scheme,
+          userPass: uri.userPass,
+          host: uri.host,
+          port: uri.port == -1 ? null : uri.port,
+          path: uri.path};
+};
 
 var resolve = exports.resolve = function resolve(base, relative) {
   var baseURI = newURI(base);
--- chrome/content/tests/test-url.js
+++ chrome/content/tests/test-url.js
@@ -1,6 +1,6 @@
-exports.testUrl = function(test) {
-  var url = require("url");
+var url = require("url");
 
+exports.testResolve = function(test) {
   test.assertEqual(url.resolve("http://www.foo.com/", "bar"),
                    "http://www.foo.com/bar");
 
@@ -15,7 +15,20 @@ exports.testUrl = function(test) {
                     "malformed URI: blah",
                     "url.resolve() should throw malformed URI on base");
 
+  test.assertRaises(function() { url.resolve("chrome://global"); },
+                    "invalid URI: chrome://global",
+                    "url.resolve() should throw invalid URI on base");
+
   test.assertEqual(url.resolve("http://www.foo.com", ""),
                    "http://www.foo.com/",
                    "url.resolve() should add slash to end of domain");
+};
+
+exports.testParse = function(test) {
+  var info = url.parse("http://foo.com/bar");
+  test.assertEqual(info.scheme, "http");
+  test.assertEqual(info.host, "foo.com");
+  test.assertEqual(info.port, null);
+  test.assertEqual(info.userPass, "");
+  test.assertEqual(info.path, "/bar");
 };
