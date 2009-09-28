bd59bb338057c93620a93e7de23c048f050a3caa
Atul Varma <varmaa@toolness.com>
2009-09-27 22:19:12 -0700

    Minor refactoring to traceback.js

--- chrome/content/lib/traceback.js
+++ chrome/content/lib/traceback.js
@@ -1,9 +1,14 @@
+// Undo the auto-parentification of URLs done in bug 418356.
+function deParentifyURL(url) {
+  return url.split(" -> ").slice(-1)[0];
+}
+
 var get = exports.get = function get() {
   var frame = Components.stack.caller;
   var stack = [];
 
   while (frame) {
-    var filename = frame.filename.split(" -> ").slice(-1)[0];
+    var filename = deParentifyURL(frame.filename);
     stack.splice(0, 0, {filename: filename,
                         lineNo: frame.lineNumber,
                         funcName: frame.name});
