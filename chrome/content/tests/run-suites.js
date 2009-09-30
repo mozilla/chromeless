9c5663bdb7e31369c4df3e5fb6d7457c0c188bb7
Atul Varma <varmaa@toolness.com>
2009-09-30 08:33:43 -0700

    Moved sandboxed loader functionality from test-cuddlefish.js to run-suites.js; it's now attached as a convenience method to the test runner.

--- chrome/content/tests/run-suites.js
+++ chrome/content/tests/run-suites.js
@@ -6,6 +6,16 @@ var SUITES = ["test-cuddlefish",
               "test-observer-service",
               "test-plain-text-console"];
 
+function makeSandboxedLoader(options) {
+  var Cuddlefish = require("cuddlefish");
+  var url = require("url");
+  var traceback = require("traceback");
+
+  var myUrl = traceback.get().slice(-1)[0].filename;
+  options.rootPaths = [myUrl, url.resolve(myUrl, "../lib/")];
+  return new Cuddlefish.Loader(options);
+}
+
 function makeTest(suite, name, test) {
   function runTest(runner) {
     console.info("executing '" + suite + "." + name + "'");
@@ -28,5 +38,6 @@ var run = exports.run = function run(onDone) {
     });
 
   var runner = new unitTest.TestRunner();
+  runner.makeSandboxedLoader = makeSandboxedLoader;
   runner.startMany({tests: tests, onDone: onDone});
 };
--- chrome/content/tests/test-cuddlefish.js
+++ chrome/content/tests/test-cuddlefish.js
@@ -1,17 +1,11 @@
 exports.testLoader = function(test) {
-  var Cuddlefish = require("cuddlefish");
-  var url = require("url");
-  var traceback = require("traceback");
-
   var prints = [];
   function print(message) {
     prints.push(message);
   }
 
-  var myUrl = traceback.get().slice(-1)[0].filename;
-  var rootPaths = [myUrl, url.resolve(myUrl, "../lib/")];
-  var loader = new Cuddlefish.Loader({rootPaths: rootPaths,
-                                      print: print});
+  var loader = test.makeSandboxedLoader({print: print});
+
   test.pass("loader instantiates within a securablemodule");
 
   loader.runScript("console.log('testing', 1, [2, 3, 4])");
