5c2ed8364f7b7e2592c6336abb9828285683436b
Atul Varma <varmaa@toolness.com>
2009-10-18 11:28:55 -0700

    Added file.dirname()

--- tests/test-file.js
+++ tests/test-file.js
@@ -1,7 +1,19 @@
 var file = require("file");
+var url = require("url");
+
+exports.testDirName = function(test) {
+  var aDir = url.toFilename("resource://gre/modules/");
+  test.assertEqual(file.dirname(aDir),
+                   aDir.slice(0, aDir.lastIndexOf("modules")-1),
+                   "file.dirname() of dir should return parent dir");
+
+  aDir = url.toFilename("resource://gre/modules/XPCOMUtils.jsm");
+  test.assertEqual(file.dirname(aDir),
+                   aDir.slice(0, aDir.lastIndexOf("XPCOM")-1),
+                   "file.dirname() of file should return its dir");
+};
 
 exports.testList = function(test) {
-  var url = require("url");
   var list = file.list(url.toFilename("resource://gre/modules/"));
   var found = [true for each (name in list)
                     if (name == "XPCOMUtils.jsm")];
