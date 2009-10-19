08881ac27700e7964c463126f000df7ce0faacf4
Atul Varma <varmaa@toolness.com>
2009-10-19 09:33:18 -0700

    Moved test-finding functionality into unit-test module.

--- tests/run-suites.js
+++ tests/run-suites.js
@@ -1,44 +1,10 @@
-var url = require("url");
+var unitTest = require("unit-test");
 var file = require("file");
-
-function makeSandboxedLoader(options) {
-  if (!options)
-    options = {};
-  var Cuddlefish = require("cuddlefish");
-
-  options.fs = Cuddlefish.loader.fs;
-  return new Cuddlefish.Loader(options);
-}
-
-function makeTest(suite, name, test) {
-  function runTest(runner) {
-    console.info("executing '" + suite + "." + name + "'");
-    test(runner);
-  }
-  return runTest;
-}
+var url = require("url");
 
 var run = exports.run = function run(options) {
-  var unitTest = require("unit-test");
-
-  var tests = [];
-
   var myDir = file.dirname(url.toFilename(__url__));
 
-  var suites = [name.slice(0, -3)
-                for each (name in file.list(myDir))
-                if (/^test-.*\.js$/.test(name))];
-
-  suites.forEach(
-    function(suite) {
-      var module = require(suite);
-      for (name in module)
-        if (name.indexOf("test") == 0)
-          tests.push(makeTest(suite, name, module[name]));
-    });
-
-  var runner = new unitTest.TestRunner();
-  runner.makeSandboxedLoader = makeSandboxedLoader;
-  runner.startMany({tests: tests,
-                    onDone: options.onDone});
+  unitTest.findAndRunTests({dirs: [myDir],
+                            onDone: options.onDone});
 };
