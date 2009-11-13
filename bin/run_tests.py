364898a64a3ab4c9f3b538b171b29fcd93f12021
Atul Varma <varmaa@toolness.com>
2009-11-13 05:56:16 -0800

    Added bin/activate, which bin/run_tests.py now assumes has been source'd.

--- bin/run_tests.py
+++ bin/run_tests.py
@@ -1,9 +1,8 @@
+#! /usr/bin/env python
+
 import sys
 import os
 import subprocess
-
-mydir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(mydir, '..', 'python-packages'))
 
 import cuddlefish
 
