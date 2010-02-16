54ae1936494d09b8554e463d6bb52f73d4d3be4c
Atul Varma <varmaa@toolness.com>
2010-02-16 10:34:45 -0800

    Refactored documentation app, made a really simple package detail view.

--- static-files/js/main.js
+++ static-files/js/main.js
@@ -1,59 +1,95 @@
-function getModules(fileStruct) {
-  var modules = [];
-  for (var name in fileStruct) {
-    if (name.match(/.*\.js$/))
-      modules.push(name.slice(0, -3));
-    else if (!('size' in fileStruct[name])) {
-      var subModules = getModules(fileStruct[name]);
-      subModules.forEach(
-        function(subModule) {
-          modules.push(name + "/" + subModule);
-        });
-    }
-  }
-  return modules;
-}
+function startApp(jQuery, window) {
+  var $ = jQuery;
+  var document = window.document;
 
-function scheduleNextIdlePing() {
-  window.setTimeout(jQuery.ajax({url:"/api/idle",
-                                 success: scheduleNextIdlePing,
-                                 error: scheduleNextIdlePing}),
-                    500);
-}
+  const NON_BREAKING_HYPHEN = "\u2011";
+  const IDLE_PING_DELAY = 500;
 
-$(window).ready(
-  function() {
-    scheduleNextIdlePing();
-    jQuery.getJSON(
-      "/api/packages",
-      function(packages) {
-        var sortedPackages = [];
-        for (name in packages)
-          sortedPackages.push(name);
-        sortedPackages.sort();
-        sortedPackages.forEach(
-          function(name) {
-            var pkg = packages[name];
-            var entry = $("#templates .entry").clone();
-            entry.find(".name").text(pkg.name);
-            entry.find(".description").text(pkg.description);
-            var libs = [];
-            if (pkg.lib) {
-              pkg.lib.forEach(
-                function(libDir) {
-                  var modules = getModules(pkg.files[libDir]);
-                  libs = libs.concat(modules);
-                });
-            }
-            var modules = entry.find(".modules");
-            libs.sort();
-            libs.forEach(
-              function(moduleName) {
-                var module = $('<li class="module"></li>');
-                module.text(moduleName);
-                modules.append(module);
-              });
-            $("#container").append(entry);
+  function getModules(fileStruct) {
+    var modules = [];
+    for (var name in fileStruct) {
+      if (name.match(/.*\.js$/))
+        modules.push(name.slice(0, -3).replace(/-/g, NON_BREAKING_HYPHEN));
+      else if (!('size' in fileStruct[name])) {
+        var subModules = getModules(fileStruct[name]);
+        subModules.forEach(
+          function(subModule) {
+            modules.push(name + "/" + subModule);
           });
+      }
+    }
+    return modules;
+  }
+
+  function makeDirTree(fileStruct) {
+    var info = $('<div class="directory"></div>');
+    for (name in fileStruct) {
+      if ('size' in fileStruct[name]) {
+        var entry = $('<div class="file"></div>');
+        entry.text(name);
+        info.append(entry);
+      } else {
+        var entry = $('<div class="file"></div>');
+        entry.text(name + "/");
+        info.append(entry);
+        info.append(makeDirTree(fileStruct[name]));
+      }
+    }
+    return info;
+  }
+
+  function showPackageInfo(pkg) {
+    var entry = $("#templates .package-detail").clone();
+    entry.find(".name").text(pkg.name);
+    entry.find(".files").append(makeDirTree(pkg.files));
+    $("#middle-column").empty().append(entry);
+  }
+
+  function processPackages(packages) {
+    var sortedPackages = [];
+    for (name in packages)
+      sortedPackages.push(name);
+    sortedPackages.sort();
+    sortedPackages.forEach(
+      function(name) {
+        var pkg = packages[name];
+        var entry = $("#templates .entry").clone();
+        entry.find(".name").text(pkg.name);
+        entry.find(".name").click(function() { showPackageInfo(pkg); });
+        entry.find(".description").text(pkg.description);
+        var libs = [];
+        if (pkg.lib) {
+          pkg.lib.forEach(
+            function(libDir) {
+              var modules = getModules(pkg.files[libDir]);
+              libs = libs.concat(modules);
+            });
+        }
+        var modules = entry.find(".modules");
+        libs.sort();
+        libs.forEach(
+          function(moduleName) {
+            var module = $('<li class="module"></li>');
+            module.text(moduleName);
+            modules.append(module);
+            modules.append(document.createTextNode(' '));
+          });
+        $("#left-column").append(entry);
       });
-  });
+  }
+
+  function sendIdlePing() {
+    jQuery.ajax({url:"/api/idle",
+                 success: scheduleNextIdlePing,
+                 error: scheduleNextIdlePing});
+  }
+
+  function scheduleNextIdlePing() {
+    window.setTimeout(sendIdlePing, IDLE_PING_DELAY);
+  }
+
+  scheduleNextIdlePing();
+  jQuery.getJSON("/api/packages", processPackages);
+}
+
+$(window).ready(function() { startApp(jQuery, window); });
