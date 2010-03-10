5a32a92f05ca4e5df375fcda84fbe4ade19fcd92
Atul Varma <varmaa@toolness.com>
2010-03-09 16:51:56 -0800

    Fixed  Bug 551307 -  Python error when running cfx test on Windows.

--- python-lib/cuddlefish/packaging.py
+++ python-lib/cuddlefish/packaging.py
@@ -110,6 +110,11 @@ def get_config_in_dir(path):
 
     return base_json
 
+def _is_same_file(a, b):
+    if hasattr(os.path, 'samefile'):
+        return os.path.samefile(a, b)
+    return a == b
+
 def build_config(root_dir, target_cfg):
     dirs_to_scan = []
 
@@ -136,7 +141,7 @@ def build_config(root_dir, target_cfg):
             pkgconfig = get_config_in_dir(path)
             if pkgconfig.name in packages:
                 otherpkg = packages[pkgconfig.name]
-                if not os.path.samefile(otherpkg.root_dir, path):
+                if not _is_same_file(otherpkg.root_dir, path):
                     raise DuplicatePackageError(path, otherpkg.root_dir)
             else:
                 packages[pkgconfig.name] = pkgconfig
