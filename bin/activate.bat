2208c290acbf6905eabf0a6a45dd6fc3568158a2
Dave Townsend <dtownsend@oxymoronical.com>
2010-05-14 11:26:49 -0700

    Bug 558505: Add python to the path where possible on windows. r=atul

--- bin/activate.bat
+++ bin/activate.bat
@@ -2,6 +2,52 @@
 set VIRTUAL_ENV=%CD%
 set CUDDLEFISH_ROOT=%VIRTUAL_ENV%
 
+SET WINCURVERKEY=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion
+REG QUERY "%WINCURVERKEY%" /v "ProgramFilesDir (x86)" >nul 2>nul
+if %ERRORLEVEL% EQU 0 (
+  SET WIN64=1
+) else (
+  SET WIN64=0
+)
+
+if "%WIN64%" EQU "1" (
+  SET PYTHONKEY=HKLM\SOFTWARE\Wow6432Node\Python\PythonCore
+) else (
+  SET PYTHONKEY=HKLM\SOFTWARE\Python\PythonCore
+)
+
+SET PYTHONVERSION=
+SET PYTHONINSTALL=
+
+if "%PYTHONVERSION%" EQU "" (
+  REG QUERY "%PYTHONKEY%\2.6\InstallPath" /ve >nul 2>nul
+  if %ERRORLEVEL% EQU 0 (
+    SET PYTHONVERSION=2.6
+  )
+)
+
+if "%PYTHONVERSION%" EQU "" (
+  REG QUERY "%PYTHONKEY%\2.5\InstallPath" /ve >nul 2>nul
+  if %ERRORLEVEL% EQU 0 (
+    SET PYTHONVERSION=2.5
+  )
+)
+
+if "%PYTHONVERSION%" EQU "" (
+  REG QUERY "%PYTHONKEY%\2.4\InstallPath" /ve >nul 2>nul
+  if %ERRORLEVEL% EQU 0 (
+    SET PYTHONVERSION=2.4
+  )
+)
+
+if "%PYTHONVERSION%" NEQ "" (
+  FOR /F "tokens=3* skip=1 delims=	 " %%A IN ('REG QUERY "%PYTHONKEY%\%PYTHONVERSION%\InstallPath" /ve') DO SET "PYTHONINSTALL=%%A"
+)
+
+if "%PYTHONINSTALL%" NEQ "" (
+  SET "PATH=%PATH%;%PYTHONINSTALL%"
+)
+
 if defined _OLD_PYTHONPATH (
     set PYTHONPATH=%_OLD_PYTHONPATH%
 )
