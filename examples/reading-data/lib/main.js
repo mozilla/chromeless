582f1529bc94b990d4607290c6c07f6df59667ee
Drew Willcoxon <adw@mozilla.com>
2010-10-01 15:46:43 -0700

    Bug 594076 - Example programs need to be able to quit automatically for automated testing purposes. r=myk

--- examples/reading-data/lib/main.js
+++ examples/reading-data/lib/main.js
@@ -2,34 +2,39 @@ var self = require("self");
 var panels = require("panel");
 var widgets = require("widget");
 
-function replace_mom(html) {
-    return html.replace("World", "Mom");
+function replaceMom(html) {
+  return html.replace("World", "Mom");
 }
-exports.replace_mom = replace_mom;
+exports.replaceMom = replaceMom;
 
 exports.main = function(options, callbacks) {
-    console.log("My ID is " + self.id);
+  console.log("My ID is " + self.id);
 
-    // Load the sample HTML into a string.
-    var hello_html = self.data.load("sample.html");
+  // Load the sample HTML into a string.
+  var helloHTML = self.data.load("sample.html");
 
-    // Let's now modify it...
-    hello_html = replace_mom(hello_html);
+  // Let's now modify it...
+  helloHTML = replaceMom(helloHTML);
 
-    // ... and then create a panel that displays it.
-    var my_panel = panels.Panel({
-        contentURL: "data:text/html," + hello_html
-    });
+  // ... and then create a panel that displays it.
+  var myPanel = panels.Panel({
+    contentURL: "data:text/html," + helloHTML
+  });
 
-    // Load the URL of the sample image.
-    var icon_url = self.data.url("mom.png");
+  // Load the URL of the sample image.
+  var iconURL = self.data.url("mom.png");
 
-    // Create a widget that displays the image.  We'll attach the panel to it.
-    // When you click the widget, the panel will pop up.
-    var my_widget = widgets.Widget({
-        label: "Mom",
-        image: icon_url,
-        panel: my_panel
-    });
-    widgets.add(my_widget);
+  // Create a widget that displays the image.  We'll attach the panel to it.
+  // When you click the widget, the panel will pop up.
+  var myWidget = widgets.Widget({
+    label: "Mom",
+    image: iconURL,
+    panel: myPanel
+  });
+  widgets.add(myWidget);
+
+  // If you run cfx with --static-args='{"quitWhenDone":true}' this program
+  // will automatically quit Firefox when it's done.
+  if (options.staticArgs.quitWhenDone)
+    callbacks.quit();
 }
--- examples/reddit-panel/lib/main.js
+++ examples/reddit-panel/lib/main.js
@@ -2,17 +2,22 @@ const widgets = require("widget");
 const panels = require("panel");
 const data = require("self").data;
 
-widgets.add(widgets.Widget({
-  label: "Reddit",
-  image: "http://www.reddit.com/static/favicon.ico",
-  panel: panels.Panel({
-    width: 240,
-    height: 320,
-    contentURL: "http://www.reddit.com/.mobile?keep_extension=True",
-    contentScriptURL: [data.url("jquery-1.4.2.min.js"), data.url("panel.js")],
-    contentScriptWhen: "ready",
-    onMessage: function(message) {
-      require("tab-browser").addTab(message);
-    }
-  })
-}));
+exports.main = function(options, callbacks) {
+  widgets.add(widgets.Widget({
+    label: "Reddit",
+    image: "http://www.reddit.com/static/favicon.ico",
+    panel: panels.Panel({
+      width: 240,
+      height: 320,
+      contentURL: "http://www.reddit.com/.mobile?keep_extension=True",
+      contentScriptURL: [data.url("jquery-1.4.2.min.js"), data.url("panel.js")],
+      contentScriptWhen: "ready",
+      onMessage: function(message) {
+        require("tab-browser").addTab(message);
+      }
+    })
+  }));
+
+  if (options.staticArgs.quitWhenDone)
+    callbacks.quit();
+};
