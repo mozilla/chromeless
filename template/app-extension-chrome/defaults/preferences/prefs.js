ae72f75d62d79853ea7b401c4f8a04b1a4af1be6
Marcio Galli <taboca@gmail.com>
2010-10-29 19:51:00 -0200

    Checking a new template that launches a more traditional XUL window app, this launches a chrome-based app with a 0x0 window that brings up the harness.js. This is based on Lloyd's investigation http://github.com/mozilla/chromeless/commit/8f26c5584d2f4f08fef1ff976657fae948cba1c5#diff-4 if you want to use this change the run script, -t argument, to -t app-extension-chrome.

---
+++ template/app-extension-chrome/defaults/preferences/prefs.js
@@ -0,0 +1,9 @@
+// Allow dump() calls to work.
+pref("browser.dom.window.dump.enabled", true);
+
+// Enable JS strict mode.
+pref("javascript.options.strict", true);
+
+// *entrypoint* xul
+pref("toolkit.defaultChromeURI", "chrome://chromeless/content/main.xul");
+
