fd1db1a95310133486e398aad3e7376ae9383883
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-11-09 16:41:34 -0700

    new content-window implementation which uses the observer service to address framebusting

---
+++ packages/chromeless/packages/chromeless-kit/lib/chromeless-sandbox-window.js
@@ -0,0 +1,85 @@
+let {Cc, Ci} = require("chrome");
+
+var gWindows = [];
+
+var ww = Cc["@mozilla.org/embedcomp/window-watcher;1"]
+    .getService(Ci.nsIWindowWatcher);
+
+observers = require("observer-service");
+
+function isTopLevelWindow(w) {
+    for (var i = 0; i < gWindows.length; i++) if (w == gWindows[i]._window) return true;
+    return false;
+}
+
+observers.add("content-document-global-created", function(subject) {
+    if (subject.window.top != subject.window.self) {
+        if (isTopLevelWindow(subject.window.parent))
+        {
+            subject.window.top = subject.window.self;
+            subject.window.parent = subject.window.self;
+        }
+        else
+        {
+            subject.window.top = subject.window.parent.top;
+        }
+    }
+});
+
+function Window(options) {
+  memory.track(this);
+
+  var features = ["",
+                  "width=" + options.width,
+                  "height=" + options.height,
+                  "centerscreen"];
+
+  if (options.titleBar == false) features.push("titlebar=no");
+
+  /* We now pass the options.url, which is the user app directly 
+  inserting it in the window, instead using the xul browser element 
+  that was here. This helped to make the session history work. 
+  */
+
+  var window = ww.openWindow(null, options.url, null, features.join(","), null);
+
+  this._id = gWindows.push(this) - 1;
+  this._window = window;
+  this._browser = null;
+  this._injector = null;
+  this.options = options;
+
+  window.addEventListener("close", this, false);
+
+  if (this.options.injectProps) {
+      for (name in this.options.injectProps) {
+          window[name] = this.options.injectProps[name];
+      }
+  }
+}
+
+Window.prototype = {
+  handleEvent: function handleEvent(event) {
+    switch (event.type) {
+    case "close":
+      if (event.target == this._window) {
+        if (gWindows[this._id])
+          delete gWindows[this._id];
+        this._window.removeEventListener("close", this, false);
+      }
+      break;
+    };
+  },
+  close: function() {
+    this._window.close();
+  }
+};
+
+require("errors").catchAndLogProps(Window.prototype, "handleEvent");
+
+exports.Window = Window;
+
+require("unload").when(
+  function() {
+    gWindows.slice().forEach(function(window) { window.close(); });
+  });
