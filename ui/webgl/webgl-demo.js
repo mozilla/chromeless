c0478ae3b8691ef355b15595045b2506031c40da
Marcio Galli <taboca@gmail.com>
2010-11-09 02:13:19 -0200

    A bit clean and note that we need to not use DOMContentLoad -- right now only the text content is rendered, we need to hook thumbnail render operation after full page load.

--- ui/webgl/webgl-demo.js
+++ ui/webgl/webgl-demo.js
@@ -227,17 +227,15 @@ function initTextures() {
 }
 
 function loopTexture() {
-
-	if(thumbImageRef != null) { 
+  if(gThumbImageRef != null) { 
       gl.bindTexture(gl.TEXTURE_2D, cubeTexture);
-      var video = thumbImageRef;
-      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, gThumbImageRef);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
       gl.generateMipmap(gl.TEXTURE_2D);
       gl.bindTexture(gl.TEXTURE_2D, null);
- 	} 
-      setTimeout("loopTexture()",300);
+  } 
+  setTimeout("loopTexture()",300);
 }
 
 function handleTextureLoaded(image, texture) {
