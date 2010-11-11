cada1059a6cf575eeb9b3cf613a58269a4b72817
Marcio Galli <taboca@gmail.com>
2010-11-11 19:06:50 -0200

    We do not need anymore the pref that requests XULRunner to launch an initial XUL window, like Firefox does it. Relates to #28

--- template/defaults/preferences/prefs.js
+++ template/defaults/preferences/prefs.js
@@ -4,6 +4,4 @@ pref("browser.dom.window.dump.enabled", true);
 // Enable JS strict mode.
 pref("javascript.options.strict", true);
 
-// *entrypoint* xul
-pref("toolkit.defaultChromeURI", "chrome://chromeless/content/main.xul");
 
