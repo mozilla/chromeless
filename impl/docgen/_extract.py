7e495fb2ee40eccdda397b3fc5f8bfeea1e7a569
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-01-21 10:28:38 -0700

    a custom documentation extractor for commonjs modules, eats JS, poops JSON

---
+++ impl/docgen/_extract.py
@@ -0,0 +1,384 @@
+#
+# DocExtractor is a module capable of reading a javascript file specified on the
+# command line, parsing documentation coments from that file (/** */) and returning
+# a data structure containing api documentation for the module.
+#
+# usage:
+#
+#   de = DocExtractor()
+#   apidocs = de.extract("somefile.js");
+#
+# Also you can run the module's unit tests by invoking it from the command line
+#
+
+import re
+import os
+
+class DocExtractor():
+    def __init__(self):
+        # the pattern for extracting a documentation block and the next line
+        self.docBlock_pat = re.compile('(/\*\*)(.*?)(\*/)([\s\n]*[^\/\n]*)?', re.S)
+
+        # after extracting the comment, fix it up (remove *s and leading spaces)
+        self.blockFilter_pat = re.compile('^\s*\* ?', re.M)
+
+        # the pattern used to split a comment block to create our token stream
+        # this will currently break if there are ampersands in the comments if there
+        # is a space before it
+        self.tokenize_pat = re.compile('^\s?(@\w\w*)', re.M);
+
+        # parse a function:
+        # @function [name]
+        # [description]
+        self.function_pat = re.compile('^(\w+)$|^(?:([\w.\[\]]+)\s*\n)?\s*(.*)$', re.S);
+
+        # parse a parameter :
+        #   @property <name> <{type}> <description>
+        self.param_pat = re.compile('^([\w.\[\]]+)\s*(?:{(\w+)})\s*(.*)$', re.S); 
+
+        # parse properties, similar to params but we also support a type
+        #   @property [name] [{type}]
+        #   [description]
+        self.prop_pat =  re.compile('^([\w.\[\]]+)?\s*(?:{(\w+)})?\s*(.*)$', re.S);
+
+        # parse returns clause (also works for @throws!)
+        # @return [{type}] [description]
+        #   or
+        # @throws [{type}] [description]
+        self.return_pat = re.compile('^\s*(?:{(\w+)})?\s*(.*)$', re.S);
+
+        # heuristic type and name guessing stuff, applied to the first non-whitespace
+        # line after the doc block.  designed for commonjs modules (note the 'exports').
+        self.findExports_pat = re.compile('^\s*exports\.(\w+)\s+', re.M);
+
+
+        self.descriptionMarker = "@description"
+        self.functionMarker = "@function"
+        self.moduleMarker = "@module"
+        self.paramMarker = "@param"
+        self.propertyMarker = "@property"
+        self.returnsMarker = "@returns"
+        self.throwsMarker = "@throws"
+        self.typeMarker = "@type"
+
+        self.markers = (
+            self.descriptionMarker,
+            self.functionMarker,
+            self.moduleMarker,
+            self.paramMarker,
+            self.propertyMarker,
+            self.returnsMarker,
+            self.throwsMarker,
+            self.typeMarker
+            )
+
+    def _isMarker(self, tok):
+        return tok in self.markers
+
+    def _popNonMarker(self, toks):
+        nxt = None
+        if not self._isMarker(self._peekTok(toks)):
+            nxt = toks.pop(0)
+        return nxt
+
+    def _peekTok(self, toks):
+        if (len(toks)):
+            return toks[0]
+        return None
+
+    def _consumeToks(self, tokens, currentObj, data):
+        cur = tokens.pop(0)
+
+        if not self._isMarker(cur):
+            raise RuntimeError("Found text where marker was expected: %s" % (cur[:20] + "..."))
+        elif cur == self.moduleMarker:
+            nxt = self._popNonMarker(tokens)
+            if nxt:
+                # nxt describes the module
+                m = self.function_pat.match(nxt)
+                if not m:
+                    raise RuntimeError("Malformed args to %s: %s" %
+                                       (self.functionMarker, (cur[:20] + "...")))
+                if m.group(1):
+                    data["module"] = name
+                else:
+                    if m.group(2):
+                        data["module"] = m.group(2)
+                    if m.group(3):
+                        if 'desc' in data:
+                            data['desc'] = data['desc'] + "\n\n" + m.group(3)
+                        else:
+                            data['desc'] = m.group(3)
+            else:
+                # in this case we'll have to guess the function name
+                pass
+
+
+        elif cur == self.functionMarker:
+            currentObj["type"] = 'function'
+            nxt = self._popNonMarker(tokens)
+            if nxt:
+                # nxt describes the function
+                m = self.function_pat.match(nxt)
+                if not m:
+                    raise RuntimeError("Malformed args to %s: %s" %
+                                       (self.functionMarker, (cur[:20] + "...")))
+                if m.group(1):
+                    currentObj['name'] = m.group(1)
+                else:
+                    if m.group(2):
+                        currentObj['name'] = m.group(2)
+                    if m.group(3):
+                        currentObj['desc'] = m.group(3)
+            else:
+                # in this case we'll have to guess the function name
+                pass
+        elif cur == self.propertyMarker:
+            currentObj["type"] = 'property'
+            nxt = self._popNonMarker(tokens)
+            if nxt:
+                # nxt now describes the property
+                m = self.prop_pat.match(nxt)
+                if not m:
+                    raise RuntimeError("Malformed args to %s: %s" %
+                                       (self.propertyMarker, (nxt[:20] + "...")))
+                if m.group(1):
+                    currentObj['name'] = m.group(1)
+                elif m.group(2):
+                    currentObj['type'] = m.group(2)
+                elif m.group(3):
+                    currentObj['desc'] = m.group(3)
+            else:
+                # in this case we'll have to guess the function name
+                pass
+        elif cur == self.descriptionMarker:
+            nxt = self._popNonMarker(tokens)
+            if nxt:
+                if 'desc' in currentObj:
+                    currentObj['desc'] = currentObj['desc'] + "\n\n" + nxt
+                else:
+                    currentObj['desc'] = nxt
+            else:
+                # XXX: should this really be fatal?
+                raise RuntimeError("@description without any body encountered")
+        elif cur == self.typeMarker:
+            nxt = self._popNonMarker(tokens)
+            if nxt:
+                currentObj['dataType'] = nxt
+            else:
+                # XXX: should this really be fatal?
+                raise RuntimeError("@type without any content encountered")
+        elif cur == self.paramMarker:
+            nxt = self._peekTok(tokens)
+            if not self._isMarker(nxt):
+                nxt = tokens.pop(0)
+                m = self.param_pat.match(nxt)
+                if not m:
+                    raise RuntimeError("Malformed args to %s: %s" %
+                                       (self.paramMarker, (nxt[:20] + "...")))
+                p = { }
+                p['name'] = m.group(1)
+                if m.group(2):
+                    p['type'] = m.group(2)
+                p['desc'] = m.group(3)
+
+                if not 'params' in currentObj:
+                    currentObj['params'] = [ ]
+                currentObj['params'].append(p)
+            else:
+                # in this case we'll have to guess the function name
+                pass
+        elif cur == self.returnsMarker:
+            nxt = self._peekTok(tokens)
+            if not self._isMarker(nxt):
+                nxt = tokens.pop(0)
+                m = self.return_pat.match(nxt)
+                if not m:
+                    raise RuntimeError("Malformed args to %s: %s" %
+                                       (self.returnsMarker, (nxt[:20] + "...")))
+                rv = { }
+                if m.group(1):
+                    rv['type'] = m.group(1)
+                rv['desc'] = m.group(2)
+
+                currentObj['returns'] = rv
+            else:
+                # in this case we'll have to guess the function name
+                pass
+        elif cur == self.throwsMarker:
+            nxt = self._peekTok(tokens)
+            if not self._isMarker(nxt):
+                nxt = tokens.pop(0)
+                # yeah, we reuse the return pattern here
+                m = self.return_pat.match(nxt)
+                if not m:
+                    raise RuntimeError("Malformed args to %s: %s" %
+                                       (self.throwsMarker, (nxt[:20] + "...")))
+                t = { }
+                if m.group(1):
+                    t['type'] = m.group(1)
+                t['desc'] = m.group(2)
+
+                if not 'throws' in currentObj:
+                    currentObj['throws'] = [ ]
+                currentObj['throws'].append(t)
+            else:
+                # in this case we'll have to guess the function name
+                pass
+        else:
+            raise RuntimeError("unrecognized tag: %s" % cur)
+
+    def _analyzeContext(self, context):
+        guessedName = None
+        guessedType = None
+        # first let's see if there's an exports statement after the block
+        m = self.findExports_pat.match(context)
+        if m:
+            guessedName = m.group(1)
+
+            # we'll only try to guess type if there's an exports statement
+            if context.find("function") >= 0: 
+                guessedType = 'function'
+            else:
+                guessedType = 'property'
+        return guessedName, guessedType
+
+    def _analyzeBlock(self, block, context, firstBlock, data):
+        tokens = self.tokenize_pat.split(block)
+
+        # remove all whitespace strings
+        tokens = [n.strip() for n in tokens if n.strip()]
+
+        curObj = {}
+
+        # special case tagless first block for module description
+        # or func/prop description
+        if not self._isMarker(tokens[0]):
+            if firstBlock:
+                data['desc'] = tokens.pop(0)
+            else:
+                curObj['desc'] = tokens.pop(0)
+
+        while len(tokens):
+            self._consumeToks(tokens, curObj, data)
+
+        (guessedName, guessedType) = self._analyzeContext(context)
+
+        if not 'name' in curObj and guessedName:
+            curObj['name'] = guessedName
+
+        if not 'type' in curObj and guessedType:
+            curObj['type'] = guessedType
+
+        if 'type' in curObj:
+            if curObj['type'] == 'function':
+                del curObj['type']
+                if 'functions' not in data:
+                    data['functions'] = [ ]
+                data['functions'].append(curObj)
+            elif curObj['type'] == 'property':
+                curObj['type'] = curObj['dataType']
+                del curObj['dataType']
+                if 'properties' not in data:
+                    data['properties'] = [ ]
+                data['properties'].append(curObj)
+            else:
+                raise RuntimeError("I don't know what to do with a: %s" % curObj['type'])
+
+    def extract(self, filename):
+        # the data structure we'll build up
+        data = {}
+
+        # first determine the module name, it's always the same as the file name
+        mod = os.path.basename(filename)
+        dotLoc = mod.rfind(".")
+        if (dotLoc > 0):
+            mod = mod[:dotLoc]
+        data["module"] = mod
+        data["filename"] = filename
+
+        # next read the whole file into memory
+        contents = ""
+        with open(filename, "r") as f:
+            contents = f.read()
+
+        # now parse out and combine comment blocks
+        firstBlock = True
+        for m in self.docBlock_pat.finditer(contents):
+            block = self.blockFilter_pat.sub("", m.group(2)).strip()
+            context = m.group(4)
+            # data will be mutated!
+            self._analyzeBlock(block, context, firstBlock, data)
+            firstBlock = False
+
+        return data
+
+if __name__ == '__main__':
+    # running this file from the command line executes its self tests.
+    import json
+    import sys
+    import difflib
+
+    de = DocExtractor()
+
+    # because docextractor embeds filenames into output files, let's
+    # change into the directory of the script for consistency
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    testDir = "tests"
+
+    # create a list of the tests to run (.js files in tests/ dir)
+    tests = []
+    for x in os.listdir(testDir):
+        if x.endswith(".js"):
+            tests.append(x[:-3])
+
+    # now run!
+    ranTests = 0
+    failedTests = 0
+    for test in tests:
+        print "Running '%s'..." % test
+        failed = False
+        got = de.extract(os.path.join(testDir, test + ".js"))
+        want = None
+        try:
+            with open(os.path.join(testDir, test + ".out"), "r") as f:
+                want = json.loads(f.read())
+        except:
+            pass
+
+        gotJSON = json.dumps(got, indent=2, sort_keys=True) + "\n"
+
+        # now let's compare actual with expected
+        if want == None:
+            print  "  FAILED: no expected test output file available (%s.out)" % test
+            failed = True
+        else:
+            wantJSON = json.dumps(want, indent=2, sort_keys=True) + "\n"
+
+            diff = difflib.unified_diff(wantJSON.splitlines(1), gotJSON.splitlines(1), "expected.out", "actual.out")
+
+            # diff does poorly when newlines are ommitted, let's fix that
+            diff = [l if len(l) > 0 and l[-1] == '\n' else l + "\n" for l in diff]
+
+            diffText = '    '.join(diff)
+
+            if len(diffText):
+                diffText = '    ' + diffText
+                print "  FAILED: actual output doesn't match expected:"
+                print diffText
+                failed = True
+            else:
+                print "  ... passed."
+
+        if failed:
+            failedTests += 1
+            # write actual output to disk, so that it's easy to write new tests
+            actualPath = os.path.join(testDir, test + ".outactual")
+            with open(actualPath, "w+") as f:
+                f.write(gotJSON)
+
+            print "  (expected output left in '%s')" % actualPath
+
+    print "Complete, (%d/%d) tests passed..." % (len(tests) - failedTests, len(tests))
+    sys.exit(failedTests)
