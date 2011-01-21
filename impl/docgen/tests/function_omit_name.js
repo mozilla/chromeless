13d639c2c0d180d3aa952c9f303890683c927669
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-01-21 16:31:07 -0700

    a pile of tests to excercise function and parameter parsing

---
+++ impl/docgen/tests/function_omit_name.js
@@ -0,0 +1,13 @@
+/**
+ * A module with a basic function definition in it,
+ * but without explicit mention of the name of the function
+ * given the heuristic name guessing stuff.
+ */
+
+/**
+ * @function
+ * a function which converts URLs to filenames
+ */
+
+let toFilename = exports.toFilename = function toFilename(url) {
+}
---
+++ impl/docgen/tests/params_type_last_newline.js
@@ -0,0 +1,12 @@
+/**
+ * A function which contains parameters expressed
+ * with name before type.
+ */
+
+/**
+ * a function which converts URLs to filenames.
+ * @param url {string}
+ * The url to convert
+ */
+let toFilename = exports.toFilename = function toFilename(url) {
+}
