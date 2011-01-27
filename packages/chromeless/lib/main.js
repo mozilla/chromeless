beca91df9f065c6bcee256218b78b76b902d7ee7
mikedeboer <info@mikedeboer.nl>
2011-01-27 10:34:52 +0100

    implemented a mapping of the uri resource://app/ to the browser_code/ directory inside the packaged chromeless app
    Signed-off-by: Lloyd Hilaiel <lloyd@hilaiel.com>

--- packages/chromeless/lib/main.js
+++ packages/chromeless/lib/main.js
@@ -41,8 +41,7 @@
 // here in this main app context. Search for Ci, we current expose Ci
 // to the developers HTML browser.
 
-const {Ci,Cc} = require("chrome");
-const file = require("file");
+const {Ci, Cc, Cr, Cu} = require("chrome");
 
 var appWindow = null; 
 
@@ -81,47 +80,64 @@ function requireForBrowser(moduleName) {
     console.log("browser HTML requires: " + moduleName);
     try {
   	    return require(moduleName);
-    } catch(e) {
-        console.log("require of '"+moduleName+"' failed: " + e);
+    }
+    catch(e) {
+        console.log("require of '" + moduleName + "' failed: " + e);
         // re-throw to the developer to give them an opportunity to handle the error
         throw e;
     }
 }
 
 exports.main = function main(options) {
-  var call = options.staticArgs;
+    var call = options.staticArgs;
 
-  var contentWindow = require("chromeless-sandbox-window");
+    var contentWindow = require("chromeless-sandbox-window");
+        // convert browser url into a resource:// url
+        // i.e. 'browser_code/index.html' is mapped to 'resource://app/index.html'
+        t           = call.browser.split("/"),
+        file        = t.pop();
+        rootPath    = call.appBasePath.replace(/[\/]+$/, "") + "/" + t.join("/"),
+        startPage   = "resource://app/" + file,
+    
+        ios         = Cc["@mozilla.org/network/io-service;1"]
+                      .getService(Ci.nsIIOService),
+        resProtocol = ios.getProtocolHandler("resource")
+                      .QueryInterface(Ci.nsIResProtocolHandler),
+    
+        environment = Cc["@mozilla.org/process/environment;1"]
+                      .getService(Ci.nsIEnvironment),
+        resRoot     = Cc["@mozilla.org/file/local;1"]
+                      .createInstance(Ci.nsILocalFile),
+    resRoot.initWithPath(rootPath);
+    
+    resProtocol.setSubstitution("app", ios.newFileURI(resRoot));
+    
+    // register chrome://* URIs
+    let cr = Cc["@mozilla.org/chrome/chrome-registry;1"]
+             .getService(Ci.nsIChromeRegistry);
+    cr.checkForNewChrome();
 
-  // convert browser url into a file url
-  var startPage = require('url').fromFilename(call.appBasePath);
-  // remove trailing slashes first
-  startPage = startPage.replace(/[\/]+$/, "") + "/" + call.browser;
+    console.log("Loading browser using = " + startPage);
 
-  console.log("Loading browser using = " + startPage);
+    /* Page window height and width is fixed, it won't be and it also
+       should be smart, so HTML browser developer can change it when
+       they set inner document width and height */
 
-  /* for now we always enable debug output to console.  At some point
-   * we should make it configurable via appinfo.json */
-  enableDebuggingOutputToConsole();
-
-  /* Page window height and width is fixed, it won't be and it also
-     should be smart, so HTML browser developer can change it when
-     they set inner document width and height */
-  appWindow = new contentWindow.Window({
-    url: startPage,
-    width: 800,
-    height: 600,
-    injectProps : {
-      require: requireForBrowser,
-      console: {
-        log: function(x) { console.log(x); }
-      },
-      exit: function() {
-        console.log("window.exit() called...");
-        appWindow.close();
-      }
-    }
-  });
+    appWindow = new contentWindow.Window({
+        url: startPage,
+        width: 800,
+        height: 600,
+        injectProps : {
+            require: requireForBrowser,
+            console: {
+                log: function(x) { console.log(x); }
+            },
+            exit: function() {
+                console.log("window.exit() called...");
+                appWindow.close();
+            }
+        }
+    });
 };
 
 exports.onUnload = function (reason) {
