5d7c4ca53f65b321fa87061110a5e9ba7a9fd80f
mikedeboer <info@mikedeboer.nl>
2011-02-07 10:33:26 +0100

    added hotkey support to register application-wide hotkeys

---
+++ packages/chromeless-kit/lib/hotkey.js
@@ -0,0 +1,99 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim:set ts=4 sw=4 sts=4 et: */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Initial Developer of the Original Code is
+ * Mike de Boer, Ajax.org.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+const {Cc, Ci, Cr} = require("chrome"),
+      ui = require("ui");
+
+/*
+shift: The Shift key.
+alt: The Alt key. On the Macintosh, this is the Option key. On Macintosh this can only be used in conjunction with another modifier, since Alt+Letter combinations are reserved for entering special characters in text.
+meta: The Meta key. On the Macintosh, this is the Command key.
+control: The Control key.
+accel: The key used for keyboard shortcuts on the user's platform, which is Control on Windows and Linux, and Command on Mac. Usually, this would be the value you would use.
+access: The access key for activating menus and other elements. On Windows, this is the Alt key, used in conjuction with an element's accesskey.
+any: Indicates that all modifiers preceding it are optional.
+*/
+const modifiers = {"shift": 1, "alt": 1, "meta": 1, "control": 1, "accel": 1, "access": 1, "any": 1};
+var keySet,
+    bindings = {};
+
+function splitSafe(s, separator, limit, bLowerCase) {
+    return (bLowerCase && (s = s.toLowerCase()) || s)
+        .replace(/(?:^\s+|\n|\s+$)/g, "")
+        .split(new RegExp("[\\s ]*" + separator + "[\\s ]*", "g"), limit || 999);
+}
+
+function getKeyset() {
+    if (keySet)
+        return keySet;
+    let doc  = ui.getMainWindow().document;
+        sets = doc.getElementsByTagName("keyset");
+    if (sets.length)
+        return (keySet = sets[0]);
+    return (keySet = doc.getElementsByTagName("window")[0].appendChild(doc.createElement("keyset")));
+}
+
+exports.register = function(id, hotkey, command) {
+    let key,
+        keys = splitSafe(hotkey, "\\-", null, true),
+        mods = [];
+    for (let i = 0, l = keys.length; i < l; ++i) {
+        key = keys[i];
+        if (modifiers[key])
+            mods.push(key), key = null;
+    }
+    if (!key)
+        throw "Please provide a valid key for a keybinding to work!";
+    // do it naively for now, without checking IF the hotkey is already registered, 
+    // but to another command.
+    let doc  = ui.getMainWindow().document,
+        node = getKeyset().appendChild(doc.createElement("key"));
+    bindings[id] = node;
+    node.setAttribute("id", id);
+    node.setAttribute("modifiers", mods.join(" "));
+    node.setAttribute("key", key);
+    if (typeof command == "string") {
+        node.setAttribute("command", command);
+    }
+    else {
+        node.setAttribute("oncommand", "this._command()");
+        node._command = command;
+    }
+};
+
+exports.unregister = function(id, hotkey, handler) {
+    
+};
