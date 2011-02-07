4072577dbabc9db409dfb35927b9e1670627be60
mikedeboer <info@mikedeboer.nl>
2011-02-07 10:34:35 +0100

    implemented hotkey.unregister and added docs

--- packages/chromeless-kit/lib/hotkey.js
+++ packages/chromeless-kit/lib/hotkey.js
@@ -37,15 +37,6 @@
 const {Cc, Ci, Cr} = require("chrome"),
       ui = require("ui");
 
-/*
-shift: The Shift key.
-alt: The Alt key. On the Macintosh, this is the Option key. On Macintosh this can only be used in conjunction with another modifier, since Alt+Letter combinations are reserved for entering special characters in text.
-meta: The Meta key. On the Macintosh, this is the Command key.
-control: The Control key.
-accel: The key used for keyboard shortcuts on the user's platform, which is Control on Windows and Linux, and Command on Mac. Usually, this would be the value you would use.
-access: The access key for activating menus and other elements. On Windows, this is the Alt key, used in conjuction with an element's accesskey.
-any: Indicates that all modifiers preceding it are optional.
-*/
 const modifiers = {"shift": 1, "alt": 1, "meta": 1, "control": 1, "accel": 1, "access": 1, "any": 1};
 var keySet,
     bindings = {};
@@ -66,7 +57,7 @@ function getKeyset() {
     return (keySet = doc.getElementsByTagName("window")[0].appendChild(doc.createElement("keyset")));
 }
 
-exports.register = function(id, hotkey, command) {
+function parseHotkey(hotkey) {
     let key,
         keys = splitSafe(hotkey, "\\-", null, true),
         mods = [];
@@ -75,15 +66,74 @@ exports.register = function(id, hotkey, command) {
         if (modifiers[key])
             mods.push(key), key = null;
     }
+    return {key: key, modifiers: mods};
+}
+
+function getBinding(hotkey, command) {
+    let bind,
+        {key, modifiers} = parseHotkey(hotkey),
+        mods = modifiers.join(" ");
+
+    for (let i in bindings) {
+        if (!bindings.hasOwnProperty(i))
+            continue;
+        bind = bindings[i];
+        if (bind.getAttribute("key") !== key && bind.getAttribute("modifiers") !== mods)
+            return;
+        if (typeof command == "string") {
+            if (bind.getAttribute("command") === command)
+                return bind;
+        }
+        else if (bind._command === command) {
+            return bind;
+        }
+    }
+}
+
+/**
+ * Register a global hotkey that executes JS specified [command] when the key
+ * combination in [hotkey] is pressed.
+ * 
+ * @throws {String} Textual exception that should be caught by the programmer.
+ * 
+ * @param {String} hotkey Key combination in the format of 'modifier-key'
+ *   Examples:
+ *     accel-s, meta-shift-i, control-alt-d
+ *   Modifier keynames:
+ *     shift: The Shift key.
+ *     alt: The Alt key. On the Macintosh, this is the Option key. On Macintosh 
+ *          this can only be used in conjunction with another modifier, since 
+ *          Alt+Letter combinations are reserved for entering special characters 
+ *          in text.
+ *     meta: The Meta key. On the Macintosh, this is the Command key.
+ *     control: The Control key.
+ *     accel: The key used for keyboard shortcuts on the user's platform, which 
+ *            is Control on Windows and Linux, and Command on Mac. Usually, this 
+ *            would be the value you would use.
+ *     access: The access key for activating menus and other elements. On Windows, 
+ *             this is the Alt key, used in conjuction with an element's accesskey.
+ *     any: Indicates that all modifiers preceding it are optional.
+ * @param {String/Function} command Javascript (may be of type String or Function) 
+ *                                  to execute when the hotkey is executed.
+ * @param {String} [id] Optional. Unique identifier for this hotkey, which will 
+ *                      auto-generated if not provided.
+ * @type  {String} Return the ID of the hotkey.
+ */
+exports.register = function(hotkey, command, id) {
+    let {key, modifiers} = parseHotkey(hotkey);
     if (!key)
         throw "Please provide a valid key for a keybinding to work!";
+    if (!modifiers.length)
+        throw "Please provide at least one modifier key for a keybinding to work!";
     // do it naively for now, without checking IF the hotkey is already registered, 
     // but to another command.
     let doc  = ui.getMainWindow().document,
         node = getKeyset().appendChild(doc.createElement("key"));
+    if (!id)
+        id = ui.getUUID();
     bindings[id] = node;
     node.setAttribute("id", id);
-    node.setAttribute("modifiers", mods.join(" "));
+    node.setAttribute("modifiers", modifiers.join(" "));
     node.setAttribute("key", key);
     if (typeof command == "string") {
         node.setAttribute("command", command);
@@ -92,8 +142,27 @@ exports.register = function(id, hotkey, command) {
         node.setAttribute("oncommand", "this._command()");
         node._command = command;
     }
+    return id;
 };
 
-exports.unregister = function(id, hotkey, handler) {
-    
+/**
+ * Register a global hotkey.
+ * @see register
+ * @throws {String} Textual exception that should be caught by the programmer.
+ * 
+ * @param {String} hotkey
+ * @param {String/Function} command
+ * @param {String} [id]
+ * @type  {void}
+ */
+exports.unregister = function(hotkey, command, id) {
+    if (id) {
+        if (!bindings[id])
+            throw "There is no hotkey registered with id '" + id + "'";
+        bindings[id].parentNode.removeChild(bindings[id]);
+    }
+    let bind = getBinding(hotkey, command);
+    if (!bind)
+        throw "Hotkey '" + hotkey + "' is not registered yet";
+    bind.parentNode.removeChild(bind);
 };
