36368392eff84c34ec65a6882fb39a16248ee52f
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-02-09 21:24:09 -0700

    whitespace only - reindent dragdrop.js

--- tests/dragdrop/dragdrop.js
+++ tests/dragdrop/dragdrop.js
@@ -1,35 +1,35 @@
-            function handleDragStart(e) {
-		var refIndex = parseInt(e.target.getAttribute("class").split("fileindex")[1]); 
-		var fullPath = require('file').join(desktopPath, filesOnDesktop[refIndex ]);
-		var fs = require('file').stat(fullPath);
+function handleDragStart(e) {
+	var refIndex = parseInt(e.target.getAttribute("class").split("fileindex")[1]); 
+	var fullPath = require('file').join(desktopPath, filesOnDesktop[refIndex ]);
+	var fs = require('file').stat(fullPath);
 
-                if(fs.type == "file") { 
-/*
+  if(fs.type == "file") { 
+    /*
   		var file = fs.file;
- 		var leafName = fs.leafName;
-*/
-                   console.log("Starting drag...to "+fullPath);
-                   var leafName = filesOnDesktop[refIndex];
-                   require('dragdrop').setDragData(e,fullPath,leafName, "copy", function(s) { console.log("success!") }, function (e) { console.log("error" + e) } );
-                   var image = document.createElement("img");
-                   image.setAttribute("border", "0");
-                   image.setAttribute("src", "images/question.png");
-                   image.setAttribute("width", "48");
-                   image.setAttribute("height", "48");
-                   e.dataTransfer.setDragImage(image, 25, 25);
-   
-               } else { 
-                   console.log("I can only associate files to the drag, for now...");
-               } 
-            }
-            
-            function handleDragEnd(e) {
-            }
-            
-            function dragEnable() {
-                var dragItems = document.querySelectorAll("[draggable=true]");
-                for (var i = 0, l = dragItems.length; i < l; ++i) {
-                    dragItems[i].addEventListener("dragstart", handleDragStart, true);
-                    dragItems[i].addEventListener("dragend", handleDragEnd, true);
-                }
-            }
+ 		  var leafName = fs.leafName;
+    */
+    console.log("Starting drag...to "+fullPath);
+    var leafName = filesOnDesktop[refIndex];
+    require('dragdrop').setDragData(e,fullPath,leafName, "copy", function(s) { console.log("success!") }, function (e) { console.log("error" + e) } );
+    var image = document.createElement("img");
+    image.setAttribute("border", "0");
+    image.setAttribute("src", "images/question.png");
+    image.setAttribute("width", "48");
+    image.setAttribute("height", "48");
+    e.dataTransfer.setDragImage(image, 25, 25);
+    
+  } else { 
+    console.log("I can only associate files to the drag, for now...");
+  } 
+}
+
+function handleDragEnd(e) {
+}
+
+function dragEnable() {
+  var dragItems = document.querySelectorAll("[draggable=true]");
+  for (var i = 0, l = dragItems.length; i < l; ++i) {
+    dragItems[i].addEventListener("dragstart", handleDragStart, true);
+    dragItems[i].addEventListener("dragend", handleDragEnd, true);
+  }
+}
