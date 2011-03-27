fec9688e4182ae40692c9a0a00108a8a4b0ac3b0
Marcio Galli <taboca@gmail.com>
2011-03-27 19:41:20 -0300

    Clean

--- tests/fullscreen/test-app.js
+++ tests/fullscreen/test-app.js
@@ -1,11 +1,8 @@
-
 const m = require("main");
 const timer = require("timer");
 
 exports.testBrowser = function (test) {
-
  var callbacks = {
-  
   onload: function () { 
        var mainBrowser= m.getAppBrowser();
        var mainWin = mainBrowser.contentWindow;
@@ -18,19 +15,13 @@ exports.testBrowser = function (test) {
          test.done();
         },500); 
        },false);
-
   }, 
-
   quit: function() {
     test.pass();
     test.done();
   } 
-
  };
 
   test.waitUntilDone();
   m.main( options, callbacks);
 }
-
-function delayedTest() { 
-} 
