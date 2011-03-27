1a0a2d3b3cc85e1b39dc10902ed6cff4fcc3e798
Marcio Galli <taboca@gmail.com>
2011-03-27 18:16:13 -0300

    onload friendly and testing that console was injected.

--- tests/console/test-app.js
+++ tests/console/test-app.js
@@ -4,25 +4,20 @@ const timer = require("timer");
 
 exports.testBrowser = function (test) {
 
- var callbacks = { quit: function() {
-    test.pass();
-    test.done();
-  } };
+ var callbacks = { 
+    onload: function () { 
+       var mainBrowser= m.getAppBrowser();
+       var appWindow = m.getAppWindow; 
+       var mainDoc = mainBrowser.contentDocument;
+       test.assert( mainBrowser.contentWindow.wrappedJSObject["console"] != null );
+       test.done();
+    }, 
+    quit: function() {
+      test.pass();
+      test.done();
+    } 
+  };
 
   test.waitUntilDone();
   m.main( options, callbacks);
-
-  timer.setTimeout( function () {
- 
-       var mainBrowser= m.getAppBrowser();
-       console.log("111" + m.getAppWindow.console);
-       var mainDoc = mainBrowser.contentDocument;
-
-       test.assert( m.getAppWindow.console.log != null );
-       test.done();
-
-  } , 5000);
-
 }
-
-
