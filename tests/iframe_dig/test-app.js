8bfcc8144829f85e5bd53d1349167e91d5289026
Marcio Galli <taboca@gmail.com>
2011-03-27 20:17:58 -0300

    Tests to iframe dig, tests if contents of remote page was changed to "hello"

---
+++ tests/iframe_dig/test-app.js
@@ -0,0 +1,22 @@
+const m = require("main");
+const timer = require("timer");
+
+exports.testBrowser = function (test) {
+ var callbacks = {
+  onload: function () {
+       var mainBrowser= m.getAppBrowser();
+       var mainDoc = mainBrowser.contentDocument;
+       mainBrowser.contentWindow.addEventListener("load", function pageLoaded() {
+         test.assert( mainDoc.getElementById("inner").contentDocument.body.innerHTML == "hello");
+         test.done();
+       },false);
+  },
+  quit: function() {
+    test.pass();
+    test.done();
+  }
+ };
+  test.waitUntilDone();
+  m.main( options, callbacks);
+}
+
