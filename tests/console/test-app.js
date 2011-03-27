ec4b5ebe44f1568e5e36e08b4e1fdaaac8800908
Marcio Galli <taboca@gmail.com>
2011-03-27 19:40:29 -0300

    clean

--- tests/console/test-app.js
+++ tests/console/test-app.js
@@ -1,14 +1,10 @@
-
 const m = require("main");
 const timer = require("timer");
 
 exports.testBrowser = function (test) {
-
  var callbacks = { 
     onload: function () { 
        var mainBrowser= m.getAppBrowser();
-       var appWindow = m.getAppWindow; 
-       var mainDoc = mainBrowser.contentDocument;
        test.assert( mainBrowser.contentWindow.wrappedJSObject["console"] != null );
        test.done();
     }, 
@@ -17,7 +13,6 @@ exports.testBrowser = function (test) {
       test.done();
     } 
   };
-
   test.waitUntilDone();
   m.main( options, callbacks);
 }
