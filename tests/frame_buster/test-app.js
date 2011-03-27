6c719684ad7b020f1537f65fc57077b6eff77f73
Marcio Galli <taboca@gmail.com>
2011-03-27 19:43:33 -0300

    clean

--- tests/frame_buster/test-app.js
+++ tests/frame_buster/test-app.js
@@ -1,32 +1,22 @@
-
 const m = require("main");
 const timer = require("timer");
 
 exports.testBrowser = function (test) {
-
  var callbacks = {
-  
   onload: function () { 
        var mainBrowser= m.getAppBrowser();
        var mainDoc = mainBrowser.contentDocument;
        var appWindow = m.getAppWindow;
-
        mainBrowser.contentWindow.addEventListener("load", function pageLoaded() { 
-  
          test.assert( mainDoc.location != mainDoc.getElementById("inner").contentDocument.location);
          test.done();
-     
        },false);
-
   }, 
-
   quit: function() {
     test.pass();
     test.done();
   } 
-
  };
-
   test.waitUntilDone();
   m.main( options, callbacks);
 }
--- tests/onload/test-app.js
+++ tests/onload/test-app.js
@@ -1,29 +1,20 @@
-
 const m = require("main");
 const timer = require("timer");
 
 exports.testBrowser = function (test) {
-
  var callbacks = {
-  
   onload: function () { 
        var mainBrowser= m.getAppBrowser();
-
        mainBrowser.contentWindow.addEventListener("load", function pageLoaded() { 
          test.assert( mainBrowser.contentDocument.body.innerHTML == "new content" );
          test.done();
        },false);
-
   }, 
   quit: function() {
     test.pass();
     test.done();
   } 
  };
-
   test.waitUntilDone();
   m.main( options, callbacks);
 }
-
-function delayedTest() { 
-} 
