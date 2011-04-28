652e581c8d7226390e08d2f3bd6b20464571d37f
Marcio Galli <taboca@gmail.com>
2011-04-28 18:22:07 -0300

    Kinda of fuzzy test, but I felt it helps. 5 seconds delay, based on expectation of network and site is up. Looks for any change in security, any cahnge in title, load, and ok ok ok values for dom-setup, load-start, and load-stop

---
+++ tests/progress_events/test-app.js
@@ -0,0 +1,25 @@
+const m = require("main");
+const timer = require("timer");
+
+exports.testBrowser = function (test) {
+ var callbacks = {
+  onload: function () { 
+       var mainBrowser= m.getAppBrowser();
+       timer.setTimeout(function () { 
+         test.assert( mainBrowser.contentDocument.getElementById("dom-setup").innerHTML == "ok" );
+         test.assert( mainBrowser.contentDocument.getElementById("load-start").innerHTML == "ok" );
+         test.assert( mainBrowser.contentDocument.getElementById("progress").innerHTML != "..." );
+         test.assert( mainBrowser.contentDocument.getElementById("security").innerHTML != "..." );
+         test.assert( mainBrowser.contentDocument.getElementById("title").innerHTML != "..." );
+         test.assert( mainBrowser.contentDocument.getElementById("status").innerHTML != "..." );
+         test.done();
+       },5000); 
+  }, 
+  quit: function() {
+    test.pass();
+    test.done();
+  } 
+ };
+  test.waitUntilDone();
+  m.main( options, callbacks);
+}
